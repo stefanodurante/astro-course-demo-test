e06ba63a1d56fd7c8417a6e464f0cdd12343ab42
sdurante <sdurante@leadtech.com>
2024-01-12 13:02:52 +0100

    Add footer and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    server: {
+        fs: {
+            // if you use this with --host anyone on the network can view
+            // the contents of your yarn cache (including private packages)
+            strict: false
+        }
+    }
+  },
   server: {
-    port: 3000,
-  }
+    port: 3000
+  },
+  integrations: [tailwind()]
 });
