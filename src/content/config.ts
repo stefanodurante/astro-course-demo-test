0ec125776a460bea1411dedcf0934ccc00b24fd3
sdurante <sdurante@leadtech.com>
2024-05-15 16:24:23 +0200

    Page collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,15 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
+import { format } from "date-fns";
 
 const postsCollection = defineCollection({
-
+  schema: z.object({
+    author: z.string(),
+    categories: z.array(z.string()),
+    date: z.string().transform((str) => format(new Date(str), "MMMM d, yyyy")),
+    featured: z.boolean().default(false),
+    image: z.string(),
+    title: z.string(),
+  }),
 });
 
 export const collections = {
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      aspectRatio: {
+        thmbnail: '1.5',
+      },
       fontFamily: {
         sans: ['Cabin', ...defaultTheme.fontFamily.sans]
       }
     },
-	},
-	plugins: [],
+  },
+  plugins: [],
 }
